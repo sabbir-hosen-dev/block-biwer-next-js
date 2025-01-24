c6a2f62d7be9da2d18bf8e1e37853e4509773bf6
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-24 13:44:04 +0600

    check profile

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,22 +1,14 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
 
-export const getServerSideProps = async ({ req }) => {
-  const { getUser } = getKindeServerSession(req);
+export default async function Profile() {
+  const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   if (!user || !user.email) {
-    return {
-      redirect: {
-        destination: '/api/auth/login',
-        permanent: false,
-      },
-    };
+    redirect('/api/auth/login');
   }
 
-  return { props: { user } };
-};
-
-export default function Profile({ user }) {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 p-6">
       <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-6 text-center">

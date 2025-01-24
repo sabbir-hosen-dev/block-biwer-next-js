9e4de4fa80688c99abc3ca6d62e517acda6c21d3
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-24 13:01:27 +0600

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,38 +4,11 @@ const Footer = () => {
   return (
     <footer className="  bg-gray-800 text-white py-6">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center md:text-left">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 text-center md:text-left">
           {/* About Section */}
           <div>
             <h2 className="text-xl font-bold">BlogView</h2>
             <p className="mt-2 text-gray-400">Simple BLog viewing web site</p>
-          </div>
-
-          {/* Quick Links */}
-          <div>
-            <h3 className="text-lg font-semibold">Quick Links</h3>
-            <ul className="mt-2 space-y-2">
-              <li>
-                <Link href="/" className="hover:text-gray-300">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href="/about" className="hover:text-gray-300">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link href="/services" className="hover:text-gray-300">
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact" className="hover:text-gray-300">
-                  Contact
-                </Link>
-              </li>
-            </ul>
           </div>
 
           {/* Social Media */}

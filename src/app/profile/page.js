bf7e1480218898a4766d3798448c27c918e2d7dc
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-24 14:03:51 +0600

    check server

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -7,15 +7,15 @@ export default async function Profile() {
 
   if (!user || !user.email) {
     redirect('/api/auth/login');
-  }
-
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 p-6">
-      <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-6 text-center">
-        <h1 className="text-2xl text-gray-800 font-semibold">
-          Welcome to your profile, {user?.email}!
-        </h1>
+  } else {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100 p-6">
+        <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-6 text-center">
+          <h1 className="text-2xl text-gray-800 font-semibold">
+            Welcome to your profile, {user?.email}!
+          </h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
